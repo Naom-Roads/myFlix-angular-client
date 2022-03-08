ce10f4e4723bb1a544eece9b6d4fb52d126fbb28
Naom-Roads <nrrodrig@gmail.com>
2022-03-08 10:12:22 +0100

    Removed unused app routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -61,7 +60,6 @@ const appRoutes: Routes = [
         MatIconModule,
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule,
         BrowserAnimationsModule,
         MatInputModule,
         MatButtonModule,
