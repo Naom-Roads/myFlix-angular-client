d1a8196ed2dc1fb5c4d78a4d2fe12bc03c4460d6
Naom-Roads <nrrodrig@gmail.com>
2022-03-10 14:00:47 +0100

    Rebuilt docs

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-flix-angular-client'),
+      dir: require('path').join(__dirname, './coverage/my-Flix-angular-client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
